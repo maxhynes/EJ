e80dc8ffa2895e3cad879e946dcecf44ed85a37e
Brenwwe <brenwwe@hotmail.com>
2013-09-30 12:23:03 +1000

    Deployer GUI and Inventory

--- EJ_common/EJ/client/interfaces/ContainerDeployer.java
+++ EJ_common/EJ/client/interfaces/ContainerDeployer.java
@@ -4,6 +4,8 @@ import EJ.tileentities.TileEntityDeployer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.Container;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
 
 public class ContainerDeployer extends Container {
 	private TileEntityDeployer deployer;
@@ -11,8 +13,63 @@ public class ContainerDeployer extends Container {
 	public ContainerDeployer(InventoryPlayer invPlayer, TileEntityDeployer entity) {
 		this.deployer = entity;
 		
+		for (int x = 0; x < 9; x++) {
+			//this.addSlotToContainer(new Slot(<inventory>, <id>, <x-pos>, <y-pos>));
+			this.addSlotToContainer(new Slot(invPlayer, x, 8 + x * 18, 142));
+		}
+		
+		for(int y = 0; y < 3; y++) {
+			for(int x = 0; x < 9; x++) {
+				//this.addSlotToContainer(new Slot(<inventory>, <id>, <x-pos>, <y-pos>));
+				this.addSlotToContainer(new Slot(invPlayer, 9 + x + y * 9, 8 + x * 18, 84 + y * 18));
+				
+			}
+		}
+			
+			for(int y = 0; y < 3; y++) {
+				for(int x = 0; x < 3; x++) {
+					//this.addSlotToContainer(new Slot(<inventory>, <id>, <x-pos>, <y-pos>));
+					addSlotToContainer(new Slot(entity, x + y * 3, 62 + x * 18, 17 + y * 18));
+					
+					
+				}
+			
+		}
+		
+		
 		
 		}
+	@Override
+	public ItemStack transferStackInSlot(EntityPlayer player, int i) {
+		Slot slot = getSlot(i);
+		if(slot != null && slot.getHasStack()) {
+			ItemStack itemstack = slot.getStack();
+			ItemStack result = itemstack.copy();
+			
+			if(i >= 36) {
+				if(!mergeItemStack(itemstack, 0, 36, false)) {
+					return null;
+				}
+			}
+			
+			else if(!mergeItemStack(itemstack, 36, 36 + deployer.getSizeInventory(), false)) {
+				return null;
+			}
+			
+			 if(itemstack.stackSize == 0) {
+				 slot.putStack(null);
+			}
+			 else
+			{	 
+				 slot.onSlotChanged();
+			}
+			 slot.onPickupFromSlot(player, itemstack); 
+			 return result;
+		}
+		
+		return null;
+	}
+	
 	@Override
 	public boolean canInteractWith(EntityPlayer player) {
 		return deployer.isUseableByPlayer(player);
--- EJ_common/EJ/tileentities/TileEntityDeployer.java
+++ EJ_common/EJ/tileentities/TileEntityDeployer.java
@@ -3,13 +3,15 @@ package EJ.tileentities;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityDeployer extends TileEntity implements IInventory  {
 	private ItemStack[] inventory;
 	
 	public TileEntityDeployer() {
-		inventory = new ItemStack[1];
+		inventory = new ItemStack[9];
 		
 	}
 
@@ -75,6 +77,42 @@ public class TileEntityDeployer extends TileEntity implements IInventory  {
 	public boolean isItemValidForSlot(int i, ItemStack itemstack) {
 		
 		return true;
+	}
+	
+	@Override
+	public void writeToNBT(NBTTagCompound compound) {
+		super.writeToNBT(compound);
+		
+		NBTTagList list = new NBTTagList();
+		
+		for(int i = 0; i < getSizeInventory(); i++) {
+			ItemStack itemstack = getStackInSlot(i);
+			
+			if(itemstack != null) {
+				NBTTagCompound item = new NBTTagCompound();
+				item.setByte("SlotDeployer", (byte) i);
+				itemstack.writeToNBT(item);
+				list.appendTag(item);
+			}
+		}
+		
+		compound.setTag("ItemsDeployer", list);
+	}
+	
+	@Override
+	public void readFromNBT(NBTTagCompound compound) {
+		super.readFromNBT(compound);
+		
+		NBTTagList list = compound.getTagList("ItemsDeployer");
+		
+		for(int i = 0; i < list.tagCount(); i++) {
+			NBTTagCompound item = (NBTTagCompound) list.tagAt(i);
+			int slot = item.getByte("SlotDeployer");
+			
+			if(slot >= 0 && slot < getSizeInventory()) {
+				setInventorySlotContents(slot, ItemStack.loadItemStackFromNBT(item));
+			}
+		}
 	}
 	
 
